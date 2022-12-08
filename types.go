6e44cff508e68f84fcc11de7f72478e0e847e9c8
Henry Peeples <hgpeeples48@gmail.com>
2022-12-07 18:27:25 -0600

    Specify JSON attribute names

--- types.go
+++ types.go
@@ -2,14 +2,16 @@ package main
 
 import "math/rand"
 
+// Account holds attributes for a user account
 type Account struct {
-	ID        int
-	FirstName string
-	LastName  string
-	Number    int64
-	Balance   int64
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Number    int64  `json:"number"`
+	Balance   int64  `json:"balance"`
 }
 
+// NewAccount creates a new Account
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
 		ID:        rand.Intn(10000),
